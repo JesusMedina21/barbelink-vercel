c14b222bf1f551d993ef71e608061417bd585d2b
JesusMedina21 <jesusmedina0921@gmail.com>
2025-03-12 16:56:57 -0400

    Update appsettings.ts

--- src/app/settings/appsettings.ts
+++ src/app/settings/appsettings.ts
@@ -1,6 +1,7 @@
 export const appsettings = {
-    apiUrl: "http://localhost:8080/api/"
-     
-     // local
+    apiUrl: "https://jereprograma.com/api/"
     
+    // local
+    //apiUrl: "http://localhost:8080/api/"
+    //
 }
